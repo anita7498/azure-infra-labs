953bb65cf2d955e30b209023bbd078a6150e8a43
anita7498 <anitarani7498@gmail.com>
2026-08-13 20:23:37 +0530

    add vnet subnet provisioning

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,17 @@
 resource "azurerm_resource_group" "dev-rg" {
-  name = "dev-iam-rg"
+  name     = "dev-iam-rg"
   location = "Central India"
+}
+resource "azurerm_virtual_network" "dev-vnet" {
+  name                = "dev-iam-vnet"
+  location            = azurerm_resource_group.dev-rg.location
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "dev-subnet" {
+  name                 = "dev-iam-subnet"
+  resource_group_name  = azurerm_resource_group.dev-rg.name
+  virtual_network_name = azurerm_virtual_network.dev-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
