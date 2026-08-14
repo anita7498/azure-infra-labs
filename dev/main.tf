c88a689e2e541c4d89997b8f02e81cf658700a89
anita7498 <anitarani7498@gmail.com>
2026-08-14 19:21:58 +0530

    added storage and container resource

--- dev/main.tf
+++ dev/main.tf
@@ -1,4 +1,18 @@
 resource "azurerm_resource_group" "dev-rg" {
-  name = "dev-iam-rg"
+  name     = "dev-iam-rg"
   location = "Central India"
+}
+
+resource "azurerm_storage_account" "dev-storage" {
+  name                     = "deviamstorageacct"
+  resource_group_name      = azurerm_resource_group.dev-rg.name
+  location                 = azurerm_resource_group.dev-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "dev-container" {
+  name                  = "iam-tfstate"
+  storage_account_id    = azurerm_storage_account.dev-storage.id
+  container_access_type = "private"
 }
