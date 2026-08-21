32a7937130aa524e605de1f141335723345a8486
anita7498 <anitarani7498@gmail.com>
2026-08-22 01:26:35 +0530

    backend configured

---
+++ infra/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+   backend "azurerm" {
+    resource_group_name  = "dev-iam-rg"
+    storage_account_name = "deviamstorageacctv1"
+    container_name       = "dev-iam-tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
